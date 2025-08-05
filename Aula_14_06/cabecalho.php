4e714c519c67bcbe483bf8658d77de0032cb284b
Julia <juliaribeiro0999@gmail.com>
2025-08-05 08:23:41 -0300

    Update cabecalho.php

--- Aula_14_06/cabecalho.php
+++ Aula_14_06/cabecalho.php
@@ -5,9 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Aulas PHP</title>
 	<link rel="icon" href="img/icon.jpg">
-	<meta name="author" content="Gabriel França">
+	<meta name="author" content="Julia">
 	<meta name="keywords" content="PHP,php,programacao,logica">
 	<meta name="description" content="Uma página PHP">
 	<link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
+
